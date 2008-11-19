b15d8eb855c762c31c5bf99d3898c332467fa974
Michail <gaganov@gmail.com>
2008-11-19 13:57:40 +0200

    home page fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,9 +4,9 @@ class PagesController < ApplicationController
     page = params[:id]
 	if File.exists?("#{RAILS_ROOT}/app/views/pages/#{page}.html.erb")
 		
-                render :action => page
+                #render :action => page
 
-                #redirect_to :action => page
+                redirect_to :action => page
         end
         
     @page = Page.find_by_permalink(params[:id])
