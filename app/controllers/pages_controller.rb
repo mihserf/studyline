f7631e9109bbf104861603b404b9993ca3cb5cf0
Michail <gaganov@gmail.com>
2008-11-24 15:37:04 +0200

    page contacts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,13 @@ class PagesController < ApplicationController
   
    def show
     page = params[:id]
+    @page = Page.find_by_permalink(params[:id])
 	if File.exists?("#{RAILS_ROOT}/app/views/pages/#{page}.html.erb")
 		
                 render :action => page
 
                 #redirect_to :action => page
         end
-        
-    @page = Page.find_by_permalink(params[:id])
 
   end
 
