705213c1c1d52942f426cc20bdfe410c06dbf84d
Michail <gaganov@gmail.com>
2008-11-26 11:26:25 +0200

    fckeditor config changed

--- public/javascripts/fckeditor/fckconfig.js
+++ public/javascripts/fckeditor/fckconfig.js
@@ -187,13 +187,15 @@ FCKConfig.DefaultFontSizeLabel = '' ;
 FCKConfig.DefaultLinkTarget = '' ;
 
 // The option switches between trying to keep the html structure or do the changes so the content looks like it was in Word
-FCKConfig.CleanWordKeepsStructure = false ;
+FCKConfig.CleanWordKeepsStructure = true ;  // changed
 
 // Only inline elements are valid.
-FCKConfig.RemoveFormatTags = 'b,big,code,del,dfn,em,font,i,ins,kbd,q,samp,small,span,strike,strong,sub,sup,tt,u,var' ;
+/*FCKConfig.RemoveFormatTags = 'b,big,code,del,dfn,em,font,i,ins,kbd,q,samp,small,span,strike,strong,sub,sup,tt,u,var' ;*/
+FCKConfig.RemoveFormatTags = '';
 
 // Attributes that will be removed
-FCKConfig.RemoveAttributes = 'class,style,lang,width,height,align,hspace,valign' ;
+/*FCKConfig.RemoveAttributes = 'class,style,lang,width,height,align,hspace,valign' ;*/
+FCKConfig.RemoveAttributes = '';
 
 FCKConfig.CustomStyles =
 {
